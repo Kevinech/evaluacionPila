648b79e1664fe84a748f4b3e7be27fa44f2e7863
Kevinech <kevinemch@uamv.edu.ni>
2025-10-20 09:14:22 -0600

    Comenzado la parte de ver tope, listbox y label

---
+++ PilaaAPP/Parte2/Metodos2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PilaaAPP.Parte2
+{
+    public class Metodos2
+    {
+
+        public void MostrarUltimo()
+        {
+            try
+            {
+
+                pila.Peek();
+
+                int elemento = pila.Peek();
+
+
+                lblPila.Text = "Ultimo elemento ingresado: " + elemento.ToString();
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Pila vacía",
+                    "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        return pila
+
+
+
+    }
+}
